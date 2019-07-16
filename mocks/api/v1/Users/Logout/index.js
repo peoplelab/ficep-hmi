98058d59d8a56f366edb62b7459b5197c368a1d7
andrea-bellini <a.bellini@peoplelab.it>
2019-07-16 12:55:18 +0200

    update logout mock

--- mocks/api/v1/Users/Logout/index.js
+++ mocks/api/v1/Users/Logout/index.js
@@ -1,5 +1,22 @@
 module.exports = {
   PUT: (req, res) => {
-    res.status(200).json(true);
+    const { authorization, session } = req.headers;
+
+    let status;
+    let response;
+
+    if (!authorization || !session) {
+      status = 401;
+      response = false;
+    } else if (authorization === global.logged.accessToken && session === global.logged.sessionId) {
+      status = 200;
+      response = true;
+    } else {
+      status = 400;
+      response = false;
+    }
+
+
+    res.status(status).json(response);
   }
 };

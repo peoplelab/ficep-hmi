f2d2a8c333cf6174c6ad4af7900b1d411ededfa9
andrea-bellini <a.bellini@peoplelab.it>
2019-07-16 12:33:59 +0200

    add mocks handlers and global variable

---
+++ mocks/global.js
@@ -0,0 +1,9 @@
+// Login
+global.login = {
+  username: 'admin1',
+  password: '123456789',
+  refreshToken: '5FS1D8G1F51D8G4RE1F15SV1S85R',
+  issuedAt: null,
+  expiredAt: null,
+  refreshExpiredAt: null,
+};
---
+++ mocks/routes.js
@@ -0,0 +1,50 @@
+const url = {
+  login: '/api/v1/Token',
+  logout: '/api/v1/Users/Logout',
+  tools: '/api/v1/odata/tools',
+  cultures: '/api/v1/Cultures',
+  fnc: '/api/v1/odata/tools/upload/fnc/paf',
+};
+
+module.exports = [
+  {
+    "url": url.login,
+    "path": `.${url.login}`,
+  },
+  {
+    "url": url.logout,
+    "path": `.${url.logout}`,
+  },
+  {
+    "url": `${url.tools}/attributes/:toolTypeId`,
+    "path": `.${url.tools}/attributes/toolTypeId`,
+  },
+  {
+    "url": `${url.tools}/categories`,
+    "path": `.${url.tools}/categories`,
+  },
+  {
+    "url": `${url.tools}/:id`,
+    "path": `.${url.tools}/id`,
+  },
+  {
+    "url": url.tools,
+    "path": `.${url.tools}`,
+  },
+  {
+    "url": `${url.tools}/Types/:categoryId`,
+    "path": `.${url.tools}/Types/categoryId`,
+  },
+  {
+    "url": url.cultures,
+    "path": `.${url.cultures}`,
+  },
+  {
+    "url": `${url.cultures}/:id`,
+    "path": `.${url.cultures}/id`,
+  },
+  {
+    "url": url.fnc,
+    "path": `.${url.fnc}`,
+  },
+];

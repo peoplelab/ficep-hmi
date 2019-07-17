dbec1fec6d2f5a05fecb481e281c8affb47c2a4d
andrea-bellini <a.bellini@peoplelab.it>
2019-07-17 13:48:45 +0200

    add Login root

---
+++ src/routes/Login/Login.index.js
@@ -0,0 +1,10 @@
+import AsyncRoute from '../../generators/AsyncRoute';
+import Component from './component/Login';
+import reducers from './controllers/Login.reducers';
+import sagas from './controllers/Login.sagas';
+
+
+const KEY = 'Login';
+
+
+export default AsyncRoute(Component, KEY, reducers, sagas);

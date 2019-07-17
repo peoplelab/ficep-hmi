258293c5934ca9ad5d21d7d118100f0c363c6f68
andrea-bellini <a.bellini@peoplelab.it>
2019-07-17 13:47:54 +0200

    add Login route component

---
+++ src/routes/Login/component/Login.container.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Login from './Login.view';
+import { onChange, callLogin } from '../controllers/Login.actions';
+import { grantType, disabled, data } from '../controllers/Login.selectors';
+
+
+const mapDispatchToProps = {
+  onChange,
+  onLogin: callLogin,
+};
+
+const mapStateToProps = state => ({
+  checked: grantType(state),
+  disabled: disabled(state),
+  username: state.Login.form.username,
+  password: state.Login.form.password,
+  refreshToken: state.Login.form.refreshToken,
+  data: data(state),
+});
+
+
+const LoginContainer = connect(mapStateToProps, mapDispatchToProps)(Login);
+
+export default withRouter(LoginContainer);
---
+++ src/routes/Login/component/Login.js
@@ -0,0 +1,5 @@
+import { hot } from 'react-hot-loader/root';
+import Login from './Login.container';
+
+
+export default hot(Login);

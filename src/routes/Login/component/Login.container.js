090c7ae14ada606ede103c3a8239f0b76162074e
andrea-bellini <a.bellini@peoplelab.it>
2019-07-17 17:20:31 +0200

    move selectors into presenters folder

--- src/routes/Login/component/Login.container.js
+++ src/routes/Login/component/Login.container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Login from './Login.view';
 import { onChange, restApiCallLogin } from '../controllers/Login.actions';
-import { grantType, disabled, data } from '../controllers/Login.selectors';
+import { grantType, disabled, data } from '../presenters';
 
 
 const mapDispatchToProps = {

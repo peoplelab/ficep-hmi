27a9b5c720e054ad7a2793d4a93947e1fdc1c84b
andrea-bellini <a.bellini@peoplelab.it>
2019-07-18 16:24:25 +0200

    update action handlers

--- src/commons/actions.js
+++ src/commons/actions.js
@@ -35,7 +35,7 @@ export const createPayloadAction = (...types) => types.reduce(reducePayloadActio
 
 const reduceApiAction = (acc, type) => {
   acc[type] = {
-    CALL: () => ({ type: `${type}_CALL` }),
+    CALL: request => ({ type: `${type}_CALL`, request}),
     OK: (status, data) => ({ type: `${type}_OK`, response: { status, data } }),
     KO: error => ({ type: `${type}_KO`, error }),
   };

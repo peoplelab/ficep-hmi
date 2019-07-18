346e6530f9022bd24615c630a2728e9c3f384a40
andrea-bellini <a.bellini@peoplelab.it>
2019-07-18 15:21:20 +0200

    fix comment

--- src/commons/AsyncComponent.jsx
+++ src/commons/AsyncComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component as ReactComponent } from 'react';
 
 /**
  * Handle asynchronous loading of React components
- * If require, it send the Redux store to the component
+ * Note: if required, it sends Redux store to component
  * @param {object} store
  */
 const AsyncComponent = store => /** @param {React} importComponent */ importComponent => (

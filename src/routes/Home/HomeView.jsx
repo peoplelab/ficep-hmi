0f3673529726ccdce1b972d0ddab107516437f8c
andrea-bellini <a.bellini@peoplelab.it>
2019-07-18 10:51:51 +0200

    fix anchor component

--- src/routes/Home/HomeView.jsx
+++ src/routes/Home/HomeView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import Anchor from '../../components/layout/Anchor';
+import Anchor from '../../components/layout/Anchor/Anchor.index';
 
 const myUrl = '/test-model';
 

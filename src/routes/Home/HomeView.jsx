31e6f1d95e7db7408b8012c0743c0a60cccd03f3
andrea-bellini <a.bellini@peoplelab.it>
2019-07-18 10:08:09 +0200

    link login from home route

--- src/routes/Home/HomeView.jsx
+++ src/routes/Home/HomeView.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import Anchor from '../../components/layout/Anchor';
 
 const myUrl = '/test-model';
 
 const HomeComponent = () => (
   <div>
     Hello world!
-	<br/>
-	<br/>
-	<br/>
-	<a href={ myUrl }> Vai a <b>TestModel</b></a>
+    <br/>
+    <br/>
+    <br/>
+    <Anchor path={ myUrl }> Vai a <b>TestModel</b></Anchor>
+    <br/>
+    <Anchor path="/login"> Vai a <b>Login</b></Anchor>
   </div>
 );
 

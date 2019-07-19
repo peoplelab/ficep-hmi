9c5f1304fcfd68dfe8c71e3834aa4666a6eb8f27
andrea-bellini <a.bellini@peoplelab.it>
2019-07-19 10:40:21 +0200

    update Login components comments

--- src/routes/Login/components/Login.selectors.js
+++ src/routes/Login/components/Login.selectors.js
@@ -1,13 +1,21 @@
 import { createSelector } from 'reselect';
 
+/**
+ * Define here the selectors to handle data to send to React component
+ */
 
+/**
+ * Disable login button if username or password are empty
+ */
 export const disabled = createSelector(
   state => state.Login.form.username,
   state => state.Login.form.password,
   (username, password) => !username || !password,
 );
 
-
+/**
+ * Show error banner only if, on login request, response return a status diffent to 200
+ */
 export const errorOnLogin = createSelector(
   state => state.session.status,
   status => status !== 200 && status !== null,

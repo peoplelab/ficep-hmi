6ebe9b97c9e4b9896aeb9d90971e024476d429a2
andrea-bellini <a.bellini@peoplelab.it>
2019-07-19 10:22:35 +0200

    update node servers comments

--- server/mocks.js
+++ server/mocks.js
@@ -1,13 +1,16 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mocks = require('../mocks');
 
 
+// localhost server port
 const PORT = 4000;
+
+// init node server
 const app = express();
 
+
+// configure and run requests body parser
 const OPTIONS = {
   URL: { extended: false },
   JSON: {
@@ -16,10 +19,13 @@ const OPTIONS = {
     type: 'application/json',
   }
 };
-
 app.use(bodyParser.urlencoded(OPTIONS.URL))
 app.use(bodyParser.json(OPTIONS.JSON));
 
+
+// inject app server to maock handler and run run mocks
 mocks(app);
 
+
+// run application on http
 app.listen(PORT);

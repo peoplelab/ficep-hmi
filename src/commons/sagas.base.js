56fdf1b141102b0bcd17bd05ccc202f677566226
andrea-bellini <a.bellini@peoplelab.it>
2019-07-22 13:01:47 +0200

    update sagas.base to handle error response text type

--- src/commons/sagas.base.js
+++ src/commons/sagas.base.js
@@ -17,20 +17,22 @@ export function* fetchData_gen(actionAPI, request, url) {
 
     //eslint-disable-next-line
     console.log('> REST API executed.');
+    console.log(response);
 
     const status = response.status;
-    const response_dataraw = yield call([response, response.json]);
 
     if (status === 200) {
       //eslint-disable-next-line
       console.log('> REST API success. Status: ' + status);
+      const response_dataraw = yield call([response, response.json]);
 
       yield put(actionAPI.SUCCESS(status, response_dataraw));
     } else {
       //eslint-disable-next-line
       console.log('> REST API error. Status: ' + status);
+      const response_dataraw = yield call([response, response.text]);
 
-      yield put(actionAPI.ERROR(status, response_dataraw));
+      yield put(actionAPI.ERROR(status, { message: response_dataraw }));
     }
 
 

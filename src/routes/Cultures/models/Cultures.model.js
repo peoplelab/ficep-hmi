87034eef050232bccd44d9e186a6180c42c8341c
andrea-bellini <a.bellini@peoplelab.it>
2019-07-23 11:46:34 +0200

    update cultures model

--- src/routes/Cultures/models/Cultures.model.js
+++ src/routes/Cultures/models/Cultures.model.js
@@ -1,56 +1,99 @@
-// import {
-//   fork
-// } from 'redux-saga/effects';
-// import { fetchData_gen } from '../../../commons/sagas.base';
+import {
+  fork
+} from 'redux-saga/effects';
+import { fetchData_gen } from '../../../commons/sagas.base';
+
+/**
+ * Rest api cultrues path
+ */
+const url = '/api/v1/Cultures';
+
+/**
+ * Set default headers
+ * @param {object} header Headers request data
+ */
+const setHeaders = header => ({
+  "Authorization": `Bearer ${header.AccessToken}`,
+  "Session": header.SessionId,
+  "Content-Type": "application/json",
+});
 
 
-// /**
-//  * Tools list custom rest api request
-//  * @param {*} action Action to call after response
-//  * @param {*} data Body request data
-//  */
-// export function* doCallToolsList(action, header) {
-//   const url = '/api/v1/odata/tools';
+/**
+ * Get cultures list
+ * @param {object} action Action to call after response
+ */
+export function* doCallCultureGet(action) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-//   const headers = {
-//     "Authorization": `Bearer ${header.AccessToken}`,
-//     "Session": header.SessionId,
-//     "Content-Type": "application/json",
-//   };
+  const request = {
+    method: "get",
+    headers,
+  };
 
-//   const request = {
-//     method: "get",
-//     headers,
-//   };
-
-//   /**
-//    * Run global rest api handler as new thread
-//    */
-//   yield fork(fetchData_gen, action, request, url);
-// }
+  /**
+   * Run global rest api handler as new thread
+   */
+  yield fork(fetchData_gen, action, request, url);
+}
 
 
-// /**
-//  * Tools details custom rest api request
-//  * @param {*} action Action to call after response
-//  * @param {*} data Body request data
-//  */
-// export function* doCallToolsDetails(action, header, id) {
-//   const url = `/api/v1/odata/tools/${id}`;
+/**
+ * Add new culture
+ * @param {object} action Action to call after response
+ * @param {object} header Headers request data
+ * @param {object} body Body request data
+ */
+export function* doCallCulturePost(action, header, body) {
+  const request = {
+    method: "post",
+    headers: setHeaders(header),
+    body,
+  };
 
-//   const headers = {
-//     "Authorization": `Bearer ${header.AccessToken}`,
-//     "Session": header.SessionId,
-//     "Content-Type": "application/json",
-//   };
+  /**
+   * Run global rest api handler as new thread
+   */
+  yield fork(fetchData_gen, action, request, url);
+}
 
-//   const request = {
-//     method: "get",
-//     headers,
-//   };
 
-//   /**
-//    * Run global rest api handler as new thread
-//    */
-//   yield fork(fetchData_gen, action, request, url);
-// }
+/**
+ * Delete a culture
+ * @param {object} action Action to call after response
+ * @param {object} header Headers request data
+ * @param {number|string} id path id param
+ */
+export function* doCallCultureDelete(action, header, id) {
+  const request = {
+    method: "delete",
+    headers: setHeaders(header),
+  };
+
+  /**
+   * Run global rest api handler as new thread
+   */
+  yield fork(fetchData_gen, action, request, `${url}/${id}`);
+}
+
+
+/**
+ * Update a culture
+ * @param {object} action Action to call after response
+ * @param {object} header Headers request data
+ * @param {object} body Body request data
+ */
+export function* doCallCulturePut(action, header, body) {
+  const request = {
+    method: "put",
+    headers: setHeaders(header),
+    body,
+  };
+
+  /**
+   * Run global rest api handler as new thread
+   */
+  yield fork(fetchData_gen, action, request, url);
+}

6fe01bf4547b9b2923bcefbd90d9d0b5b686717b
andrea-bellini <a.bellini@peoplelab.it>
2019-07-23 12:57:34 +0200

    update cultures components

--- src/routes/Cultures/components/Cultures.container.js
+++ src/routes/Cultures/components/Cultures.container.js
@@ -2,14 +2,17 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Cultures from './Cultures.view';
 import { action } from '../controllers/Cultures.actions';
-// import { JsonToString } from './Cultures.presenter';
-// import { stateOr } from '../../../commons/selectors';
 
 
 const mapDispatchToProps = {
+  getCultures: action.RESTAPI_CULTURES_GET_CALL,
+  addCulture: action.RESTAPI_CULTURES_POST_CALL,
+  removeCulture: action.RESTAPI_CULTURES_DELETE_CALL,
+  updateCulture: action.RESTAPI_CULTURES_PUT_CALL,
 };
 
 const mapStateToProps = state => ({
+  list: state.Cultures.data,
 });
 
 

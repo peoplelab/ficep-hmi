64b489d44764cca070c546c8d5d8b02d226a57b4
andrea-bellini <a.bellini@peoplelab.it>
2019-09-05 16:50:56 +0200

    add form context handlers

---
+++ src/components/forms-context/GetStore.jsx
@@ -0,0 +1,34 @@
+//------------------------------------------------------------------------------------------------------
+// File: GetStore.jsx
+//
+// Desc: Espone l'oggetto state, dello store context del componente padre, al componente React chiamante
+// Path: /src/components/forms/GetStore
+//------------------------------------------------------------------------------------------------------
+
+
+import React, { Fragment, memo, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FormContext } from '../../store/form.store';
+
+
+const GetStore = (props) => {
+  const {
+    getter,
+  } = props;
+
+  const [state] = useContext(FormContext);
+
+  getter(state);
+
+  return <Fragment />;
+};
+
+GetStore.propTypes = {
+  getter: PropTypes.func.isRequired,
+};
+
+GetStore.defaultProps = {
+};
+
+
+export default memo(GetStore);
---
+++ src/components/forms-context/SetStore.jsx
@@ -0,0 +1,42 @@
+//-------------------------------------------------------------------------------------------
+// File: SetStore.jsx
+//
+// Desc: Innietta nei componenti figli il dispatcher dello store context del componente padre
+//       Da utilizzare per componenti form custom
+// Path: /src/components/forms/SetStore
+//-------------------------------------------------------------------------------------------
+
+
+import React, { memo, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FormContext } from '../../store/form.store';
+
+
+const SetStore = (props) => {
+  const { children } = props;
+
+  const [, dispatch] = useContext(FormContext);
+
+  const newChildren = React.Children.map(children, child => {
+    if (!(React.isValidElement(child))) {
+      return;
+    }
+
+    return React.cloneElement(child, { setter: dispatch });
+  });
+
+  return newChildren;
+};
+
+SetStore.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element),
+  ]).isRequired,
+};
+
+SetStore.defaultProps = {
+};
+
+
+export default memo(SetStore);

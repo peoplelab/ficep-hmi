778e1fb14822b3518e0b8cb20354957d97d3c50e
andrea-bellini <a.bellini@peoplelab.it>
2019-09-06 15:48:17 +0200

    update logged template code and style

--- src/components/templates/logged.view.jsx
+++ src/components/templates/logged.view.jsx
@@ -6,78 +6,93 @@
 //----------------------------------------------------------------------------------------
 
 
-import React, { memo } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Anchor from '../layouts/Anchor';
-
-import dashboard from '../../../public/icons/ic-dashboard.svg';
-import programs from '../../../public/icons/ic-folder.svg';
-import settings from '../../../public/icons/ic-settings.svg';
-import tools from '../../../public/icons/ic-tools.svg';
+import Button from '../layouts/Button';
 
 import '../../styles/templates/logged.style.scss';
 
 
-const HomeRoute = (props) => {
-  const { children } = props;
+class LoggedTemplate extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="logged">
-      <header className="logged__header">
-        HEADER
-      </header>
-      <section className="logged__container">
-        <div className="logged__route">
-          {children}
-        </div>
-        <nav className="logged__navbar">
-          <ul className="logged__list">
-            <li className="logged__item">
-              <Anchor className="logged__anchor anchor--tile" path="/">
-                <i className="anchor__icon ic-dashboard" />
-              </Anchor>
-            </li>
-            <li className="logged__item">
-              <Anchor className="logged__anchor anchor--tile" path="/programs">
-                <i className="anchor__icon ic-programs" />
-              </Anchor>
-            </li>
-            <li className="logged__item">
-              <Anchor className="logged__anchor anchor--tile" path="/tools">
-                <i className="anchor__icon ic-tools" />
-              </Anchor>
-            </li>
-            <li className="logged__item">
-              <Anchor className="logged__anchor anchor--tile" path="/settings">
-                <i className="anchor__icon ic-settings" />
-              </Anchor>
-            </li>
-          </ul>
-          <div>
-            Item
+    this.state= {
+      reverse: false
+    };
+
+    this.onReverse = this.onReverse.bind(this);
+  }
+
+  onReverse() {
+    this.setState((prevState) => ({ reverse: !(prevState.reverse) }));
+  }
+
+  render() {
+    const { children } = this.props;
+
+    const containerClass = `logged__container ${this.state.reverse ? 'logged__navbar--reverse' : ''}`;
+    const reverseClass = `logged__reverse-icon ${this.state.reverse ? 'ic-right-hand' : 'ic-left-hand'}`;
+
+    return (
+      <div className="logged">
+        <header className="logged__header">
+          HEADER
+        </header>
+        <section className={containerClass}>
+          <div className="logged__route">
+            {children}
           </div>
-        </nav>
-      </section>
-      <footer className="logged__footer">
-        FOOTER
-      </footer>
-    </div>
-  );
-};
+          <nav className="logged__navbar">
+            <ul className="logged__list">
+              <li className="logged__item">
+                <Anchor className="logged__anchor anchor--tile" path="/">
+                  <i className="anchor__icon ic-dashboard" />
+                </Anchor>
+              </li>
+              <li className="logged__item">
+                <Anchor className="logged__anchor anchor--tile" path="/programs">
+                  <i className="anchor__icon ic-programs" />
+                </Anchor>
+              </li>
+              <li className="logged__item">
+                <Anchor className="logged__anchor anchor--tile" path="/tools">
+                  <i className="anchor__icon ic-tools" />
+                </Anchor>
+              </li>
+              <li className="logged__item">
+                <Anchor className="logged__anchor anchor--tile" path="/settings">
+                  <i className="anchor__icon ic-settings" />
+                </Anchor>
+              </li>
+            </ul>
+            <Button className="logged__reverse" onClick={this.onReverse}>
+              <i className={reverseClass} />
+            </Button>
+          </nav>
+        </section>
+        <footer className="logged__footer">
+          FOOTER
+        </footer>
+      </div>
+    );
+  }
+}
 
 
 /**
  * Define component properties types
  */
-HomeRoute.propTypes = {
+LoggedTemplate.propTypes = {
   children: PropTypes.element.isRequired
 };
 
 /**
  * Define default value of component properties
  */
-HomeRoute.defaultProps = {
+LoggedTemplate.defaultProps = {
 };
 
 
-export default memo(HomeRoute);
+export default LoggedTemplate;

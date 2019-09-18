fd3b03a0df8d490ee603b1c353de13c6a094437e
andrea-bellini <a.bellini@peoplelab.it>
2019-09-18 12:19:23 +0200

    update redux store logs

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,14 +1,19 @@
 // Log di tutte le modifiche allo stato dello store e delle action passate allo store.dispatch
 export const logger = store => next => action => {
-  console.group(action.type);
-  console.log('> prev state', store.getState());
-  console.log('> dispatching', action);
+  const prevTime = new Date();
+  const prevState = store.getState();
 
   // esecuzione della action e modifica dello stato dello store
   let result = next(action);
 
-  console.log('> next state', store.getState());
-  console.groupEnd();
+  const nextTime = new Date();
+  const nextState = store.getState();
 
+  console.group('STORE', action.type);
+  console.table({
+    prev: { time: prevTime, action, state: prevState },
+    next: { time: nextTime, action: result, state: nextState },
+  });
+  console.groupEnd();
   return result;
 };

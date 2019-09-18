ffbb71dd621ea3544be8855652501bf89340ee54
andrea-bellini <a.bellini@peoplelab.it>
2019-09-18 12:00:59 +0200

    fix proxy handler logs

--- server/app/app.proxy.js
+++ server/app/app.proxy.js
@@ -2,31 +2,42 @@ const proxy = require('express-http-proxy');
 
 // proxy handler (logging requests)
 const proxyOpts = ({ URL, LOG_LEVEL, route }) => {
-  let proxyReqPathResolver = (req) => route + req.url;
+  let proxyReqPathResolver = async (req) => route + req.url;
   let proxyReqBodyDecorator;
   let userResDecorator;
 
   if (LOG_LEVEL === 'debug') {
 
-    const request = {};
-    let bodyRequest = {};
 
-    proxyReqBodyDecorator = (bodyContent, srcReq) => {
-      request.time = new Date();
-      request.method = srcReq.method;
-      request.url = URL;
-      request.path = route + srcReq.path;
-      bodyRequest = bodyContent.toString('utf8');
+    proxyReqBodyDecorator = async (bodyContent, srcReq) => {
+      const request = {
+        time: new Date(),
+        method: srcReq.method,
+        url: URL,
+        path: route + srcReq.path,
+      };
+      const bodyRequest = bodyContent.toString('utf8');
+
+      console.group('\x1b[33m', 'REQUEST', '\t', URL + request.path, '\x1b[0m');
+      console.table({ request });
+      console.log('\x1b[36m', '\t', 'Request Body', '\n', bodyRequest, '\x1b[0m', '\n');
+      console.groupEnd();
+
       return bodyContent;
     };
 
-    userResDecorator = (proxyRes, proxyResData, userReq, userRes) => {
-      const response = { time: new Date() };
+    userResDecorator = async (proxyRes, proxyResData, userReq, userRes) => {
+      const response = {
+        time: new Date(),
+        method: userReq.method,
+        url: URL,
+        path: route + userReq.path,
+        statusCode: userRes.statusCode,
+        // statusMessage: userRes.statusMessage,
+      };
       const bodyResponse = proxyResData.toString('utf8');
 
-      console.group('\x1b[33m', 'API', '\t', URL + request.path, '\x1b[0m');
-      console.table({ request });
-      console.log('\x1b[36m', '\t', 'Request Body', '\n', bodyRequest, '\x1b[0m', '\n');
+      console.group('\x1b[33m', 'RESPONSE', '\t', URL + response.path, '\x1b[0m');
       console.table({ response });
       console.log('\x1b[36m', '\t', 'Response Body', '\n', bodyResponse, '\x1b[0m', '\n');
       console.groupEnd();

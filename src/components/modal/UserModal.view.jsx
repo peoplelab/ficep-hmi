936de5b06c1f17c3e76ab7ce95238640da210067
andrea-bellini <a.bellini@peoplelab.it>
2019-09-19 11:41:33 +0200

    change area component path

--- src/components/modal/UserModal.view.jsx
+++ src/components/modal/UserModal.view.jsx
@@ -9,7 +9,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '../layouts/index.layouts';
-import AdminArea from '../common/Area.admin';
+import AdminArea from '../common/Area/Area.admin';
 import { callLogout } from '../../controllers/routes/logout/logout.controller';
 
 import '../../styles/modal/UserModal.style.scss';

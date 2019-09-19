4aca462a1fa90b208368072531013367fa88fdbf
andrea-bellini <a.bellini@peoplelab.it>
2019-09-19 10:24:20 +0200

    fix get user api mock

--- mocks/api/v1/odata/users/index.js
+++ mocks/api/v1/odata/users/index.js
@@ -1,7 +1,8 @@
+const { base } = require('../../../../mock.base');
 const responseJSON = require('./response.json');
 
 module.exports = {
-  GET: (req, res) => {
-    res.status(200).json(responseJSON);
-  },
+  GET: base(
+    (req, res) => responseJSON
+  ),
 };

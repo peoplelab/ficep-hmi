b054ca0f805eefe3ebe1588e58b8aa34ee95f855
andrea-bellini <a.bellini@peoplelab.it>
2019-09-20 10:53:24 +0200

    update sandbox user route

--- src/sandbox/_users/users.view.jsx
+++ src/sandbox/_users/users.view.jsx
@@ -8,10 +8,11 @@
 import React, { PureComponent } from 'react';
 // import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader/root';
-import Box from '../../components/layouts/Box';
 import Button from '../../components/layouts/Button';
-import List from './users.item.list';
-import { callUsersList } from '../../controllers/routes/users/users.controller';
+import ButtonData from '../../components/layouts/ButtonData';
+import { } from '../../controllers/routes/users/users.controller';
+import { } from '../../controllers/routes/users/groups.controller';
+import { } from '../../controllers/api/session.controller';
 
 import '../style/users.style.scss';
 
@@ -20,44 +21,154 @@ class UsersRoute extends PureComponent {
 	constructor(props) {
     super(props);
 
-    // inizializzazione dello stato della pagina
-    this.state = {
-      list: [],
-      error: null,
-      failure: null,
-    };
-
-    this.updateState = this.updateState.bind(this);
-    this.onCallList = this.onCallList.bind(this);
+    this.getusersList = this.getusersList.bind(this);
+    this.getuserDetail = this.getuserDetail.bind(this);
+    this.exportUser = this.exportUser.bind(this);
+    this.getGroupsList = this.getGroupsList.bind(this);
+    this.getPermissionsList = this.getPermissionsList.bind(this);
+    this.addUserToGroup = this.addUserToGroup.bind(this);
+    this.removeUserFromGroup = this.removeUserFromGroup.bind(this);
+    this.checkSession = this.checkSession.bind(this);
   }
 
-  // chimata per aggiornare lo stato corrente della pagina
-  updateState(newState) {
-    this.setState(newState);
+  getusersList(event) {
+
   }
 
-  // richiesta della lista degli utenti
-  onCallList() {
-    const dispatch = this.updateState;
+  getuserDetail(event) {
+    const { data } = event;
+  }
 
-    callUsersList({ dispatch });
+  exportUser(event) {
+
+  }
+
+  getGroupsList(event) {
+
+  }
+
+  getPermissionsList(event) {
+
+  }
+
+  addUserToGroup(event) {
+
+  }
+
+  removeUserFromGroup(event) {
+
+  }
+
+  checkSession(event) {
+
   }
 
   // renderizzazione della pagina
 	render() {
+    const url = 0;
+    const request = 0;
+    const response = 0;
+
     return (
         <section className="users">
           <h1 className="users__title">
             Users
           </h1>
-          <Box className="users__container">
-            <Box className="users__group">
-              <Button className="users__button" onClick={this.onCallList}>
-                Get users list
-              </Button>
-            </Box>
-            <List usersSetState={this.updateState} usersGetState={this.state} />
-          </Box>
+          <div className="users__container">
+            <h2 className="users__sub-title">Actions</h2>
+            <div className="users__section users__container">
+              <div className="users__section users__actions">
+                <h3>Users</h3>
+                <ul>
+                  <li>
+                    <Button className="users__action-button" onClick={this.getusersList}>
+                      List
+                    </Button>
+                  </li>
+                  <li>
+                    <ButtonData className="users__action-button" onClick={this.getuserDetail} data={1}>
+                      Details admin
+                    </ButtonData>
+                  </li>
+                  <li>
+                    <ButtonData className="users__action-button" onClick={this.getuserDetail} data={2}>
+                      Details super
+                    </ButtonData>
+                  </li>
+                  <li>
+                    <ButtonData className="users__action-button" onClick={this.getuserDetail} data={3}>
+                      Details user
+                    </ButtonData>
+                  </li>
+                  <li>
+                    <Button className="users__action-button" onClick={this.exportUser}>
+                      Export
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+              <div className="users__section users__actions">
+                <h3>Groups</h3>
+                <ul>
+                  <li>
+                    <Button className="users__action-button" onClick={this.getGroupsList}>
+                      List
+                    </Button>
+                  </li>
+                  <li>
+                    <Button className="users__action-button" onClick={this.getPermissionsList}>
+                      Permissions
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+              {/* <div className="users__section users__actions">
+                <h3>Users and groups</h3>
+                <ul>
+                  <li>
+                    <Button className="users__action-button" onClick={this.addUserToGroup}>
+                      Add
+                    </Button>
+                  </li>
+                  <li>
+                    <Button className="users__action-button" onClick={this.removeUserFromGroup}>
+                      Delete
+                    </Button>
+                  </li>
+                </ul>
+              </div> */}
+              <div className="users__section users__actions">
+                <h3>Session</h3>
+                <ul>
+                  <li>
+                    <Button className="users__action-button" onClick={this.checkSession}>
+                      Check
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="users__section users__container">
+              <div className="users__section users__data">
+                <h2 className="users__sub-title">URL</h2>
+                <div className="users__code">
+                  {url}
+                </div>
+              </div>
+              <div className="users__section users__data">
+                <h2 className="users__sub-title">Request</h2>
+                <div className="users__code">
+                  {request}
+                </div>
+              </div>
+              <div className="users__section users__data">
+                <h2 className="users__sub-title">Response</h2>
+                <div className="users__code">
+                  {response}
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
     );
 	}

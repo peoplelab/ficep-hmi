0502e960ce85a395a790f819d1f07ab54a66b056
andrea-bellini <a.bellini@peoplelab.it>
2019-10-23 10:15:50 +0200

    fix modal sandbox

--- src/sandbox/_modal/modal.view.jsx
+++ src/sandbox/_modal/modal.view.jsx
@@ -18,39 +18,55 @@ class ModalRoute extends PureComponent {
 	constructor(props) {
     super(props);
 
-    this.state = { open: false };
+    this.state = {
+      confirmOpen: false,
+      errorOpen: false,
+      infoOpen: false,
+      successOpen: false,
+    };
 
     this.onOpen = this.onOpen.bind(this);
     this.onClose = this.onClose.bind(this);
     this.onConfirm = this.onConfirm.bind(this);
   }
 
-  onOpen() {
-    this.setState(prev => ({ open: true }));
+  onOpen = (key) => () => {
+    this.setState({ [key]: true });
   }
 
-  onClose() {
-    this.setState(prev => ({ open: false }));
+  onClose = (key) => () => {
+    this.setState({ [key]: false });
   }
 
   onConfirm() {
     alert('HI!');
     console.log('HI!');
-    this.setState(prev => ({ open: false }));
+    this.setState({ confirmOpen: false });
   }
 
 	render() {
 
     return (
         <section className="test">
-          <Button onClick={this.onOpen} >Open modal</Button>
+          <Button onClick={this.onOpen('confirmOpen')} >Open modal confirm</Button>
+          <br />
+          <br />
+          <Button onClick={this.onOpen('errorOpen')} >Open modal error</Button>
+          <br />
+          <br />
+          <Button onClick={this.onOpen('infoOpen')} >Open modal info</Button>
+          <br />
+          <br />
+          <Button onClick={this.onOpen('successOpen')} >Open modal success</Button>
+          <br />
+          <br />
           {/* <Dialog open={this.state.open} onClose={this.onClose} /> */}
-          <ConfirmModal open={this.state.open} onClose={this.onClose} onConfirm={this.onConfirm} />
-          {/* <ErrorsModal open={this.state.open} onClose={this.onClose} errorCode={'TEST_GENERIC_ERROR'} errorsList={['TEST_SPECIFIC_ERROR']} /> */}
-          {/* <InfoModal open={this.state.open} onClose={this.onClose}>
+          <ConfirmModal open={this.state.confirmOpen} onClose={this.onClose('confirmOpen')} onConfirm={this.onConfirm} />
+          <ErrorsModal open={this.state.errorOpen} onClose={this.onClose('errorOpen')} errorCode={'TEST_GENERIC_ERROR'} errorsList={['TEST_SPECIFIC_ERROR']} />
+          <InfoModal open={this.state.infoOpen} onClose={this.onClose('infoOpen')}>
             TEST_INFO
-          </ InfoModal> */}
-          {/* <SucessModal open={this.state.open} onClose={this.onClose} /> */}
+          </ InfoModal>
+          <SucessModal open={this.state.successOpen} onClose={this.onClose('successOpen')} />
         </section>
     );
 	}

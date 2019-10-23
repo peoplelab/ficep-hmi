0a145bc37f07fe9c9796d27c4771914bdc618780
andrea-bellini <a.bellini@peoplelab.it>
2019-10-23 11:09:50 +0200

    update error handler controller and add default errors translations

--- src/controllers/common/error.handler.js
+++ src/controllers/common/error.handler.js
@@ -1,16 +1,15 @@
-import store from '../../store/redux.store';
-import { types } from '../../store/session.store';
-
-
 export const errorHandler = ({ request, contentType, dataprocessed}) => {
   if (contentType.includes("application/json")) {
     const { responseType } = dataprocessed;
 
-    if (!!responseType && responseType !== 200) {
-      store.dispatch({
-        type: types.SET_ERROR,
-        payload: dataprocessed,
-      });
+    if (!responseType || responseType === 200) {
+      //
+    } else if (/^4\d{2}$/.test(responseType.toString())) {
+      dataprocessed.errorCode = dataprocessed.errorCode || "GENERIC_TECHNICAL_ERROR";
+    } else if (/^5\d{2}$/.test(responseType.toString())) {
+      dataprocessed.errorCode = "GENERIC_SERVER_ERROR";
     }
+
+    return { request, contentType, dataprocessed };
   }
 };

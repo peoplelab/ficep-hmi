0c2715326df93c826d39441e6d72fa06500c081e
andrea-bellini <a.bellini@peoplelab.it>
2019-10-24 16:00:01 +0200

    update documentation and wiki

--- src/controllers/common/modal.handler.js
+++ src/controllers/common/modal.handler.js
@@ -1,7 +1,21 @@
+//----------------------------------------------------------------------------------------
+// File: modal.handler.js
+//
+// Desc: Funzione di gestione globale delle modali
+//----------------------------------------------------------------------------------------
+
+
 import store from '../../store/redux.store';
 import { types } from '../../store/modal.store';
 
 
+// Private Methods
+
+// Gestore dell'apertura della modale di errore e dei relativi dati
+//
+// input:
+//    ° errorCode: codice dell'errore generico
+//    ° errorsList: lista dei codici di errore specifici
 const errorHanlder = ({ errorCode, errorsList }) => {
   store.dispatch({
     type: types.OPEN_ERROR_MODAL,
@@ -15,6 +29,7 @@ const errorHanlder = ({ errorCode, errorsList }) => {
   });
 };
 
+// Gestore dell'apertura della modale di successo
 const successHanlder = () => {
   store.dispatch({
     type: types.OPEN_SUCCESS_MODAL,
@@ -24,6 +39,10 @@ const successHanlder = () => {
   });
 };
 
+// Gestore dell'apertura della modale informativa
+//
+// input:
+//    ° message: stringa delle informazioni da visualizzare
 const infoHanlder = ({ message }) => {
   store.dispatch({
     type: types.OPEN_INFO_MODAL,
@@ -34,6 +53,10 @@ const infoHanlder = ({ message }) => {
   });
 };
 
+// Gestore dell'apertura della modale di conferma
+//
+// input:
+//    ° onConfirm: funzione callback da avviare all'OK dell'utente
 const confirmHanlder = ({ onConfirm }) => {
   store.dispatch({
     type: types.OPEN_CONFIRM_MODAL,
@@ -44,12 +67,15 @@ const confirmHanlder = ({ onConfirm }) => {
   });
 };
 
+// Gestore della chusura delle modali
 const closeHandler = () => {
   store.dispatch({
     type: types.CLOSE_MODAL
   });
 };
 
+
+// Interface
 
 export const ModalHandler = {
   Error: errorHanlder,

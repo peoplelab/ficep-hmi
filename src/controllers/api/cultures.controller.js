95d57b71362c83eaf4df6cc38b661c4313097686
HassanPeoplelab <h.hassan@peoplelab.it>
2019-10-29 15:52:48 +0100

    [Ficep_HMI] Cultures View

--- src/controllers/api/cultures.controller.js
+++ src/controllers/api/cultures.controller.js
@@ -3,16 +3,6 @@
 //
 // Path: /src/controllers/api/cultures.controller
 //----------------------------------------------------------------------------------------
-
-
-//import {
-//  apiCultureGet,
-//  apiCulturePost,
-//  apiCultureDelete,
-//  apiCulturePut
-//} from '../../models/api/cultures.model';
-//import { base } from '../common/controller.base';
-
 import {
     Cultures as mCultures
 } from '../../models/api/cultures.model';
@@ -20,36 +10,9 @@ import {
 import { base } from '../common/controller.base';
 
 
-
-
-
-//// richiesta per il recupero della lista delle culture da passare alla view
-//export const callCulturesGet = async ({ dispatch }) => {
-//    base({
-//        api: apiCultureGet,
-//        success: ({ dataprocessed }) => {
-//            const data = {
-//                id: dataprocessed.result.id,
-//                code: dataprocessed.result.code,
-//                description: dataprocessed.result.description,
-//            };
-
-//            dispatch({ data });
-//        },
-//        failure: () => {
-//            dispatch({ data: [] });
-//        },
-//        refresh: false
-//    });
-//};
-
-
-
-
-
 // Interface
 export const Cultures = {
-    GetList: (dispatch) => { return callCulturesGet(dispatch); }                    // Lista Gruppi
+    GetList: (dispatch) => { return callCulturesGet(dispatch); }                    // Lista Culture(Lingue)
 };
 
 
@@ -58,7 +21,7 @@ const callCulturesGet = async ({ dispatch }) => {
     // recupera la lista degli utenti da inviare alla view
     base({
 
-        api: mCultures,
+        api: mCultures.List,
 
         success: ({ dataprocessed }) => {
             const cultures = dataprocessed.result.map(item => ({
@@ -78,120 +41,3 @@ const callCulturesGet = async ({ dispatch }) => {
     });
 };
 
-
-
-
-////////////Da qui in poi si puo cancellare tutto//////////////////////////////////////
-
-//// Gestione e conversione dei dati grezzi della response da inviare alla view
-//const dataPost = (state, payload) => {
-//  const { data } = state;
-
-//  const newData = [...data, {
-//    id: payload.id,
-//    code: payload.code,
-//    description: payload.description,
-//  }];
-
-//  return { ...state, data: newData };
-//};
-
-//// Gestione e conversione dei dati grezzi della response da inviare alla view
-//const dataDelete = (state, payload) => {
-//  if (!payload) {
-//    return state;
-//  }
-
-//  const { data } = state;
-//  const newData = data.filter(({ id }) => id !== payload.id);
-
-//  return { ...state, data: newData };
-//};
-
-//// Gestione e conversione dei dati grezzi della response da inviare alla view
-//const dataPut = (state, payload) => {
-//  if (!payload) {
-//    return state;
-//  }
-
-//  let { data } = state;
-
-//  const index = data.findIndex(item => `${item.id}` === payload.id);
-//  const before = data.slice(0, index);
-//  const after = data.slice(index + 1, data.length);
-
-//  const newData = [...before, {
-//    id: payload.id,
-//    code: payload.code,
-//    description: payload.description,
-//  }, ...after];
-
-//  return { ...state, data: newData };
-//};
-
-
-//// richiesta per aggiungere una nuova cultura, indicata precedentemente nella view
-//export const callCulturesPost = async ({ data, dispatch, state }) => {
-//  const arg = {
-//    request: {
-//      code: data.code,
-//      description: data.description,
-//    },
-//    api: apiCulturePost,
-//    success: ({ dataprocessed }) => {
-//      dispatch(dataPost(state, { ...arg.request, id: dataprocessed.result }));
-//    },
-//    failure: ({ dataprocessed, error }) => {
-//      dispatch({ error: dataprocessed || error });
-//    }
-//  };
-
-//  base(arg);
-//};
-
-//// richiesta per eliminare una cultura, indicata precedentemente nella view
-//export const callCulturesDelete = async ({ data, dispatch, state }) => {
-//  const arg = {
-//    params: {
-//      id: data,
-//    },
-//    api: apiCultureDelete,
-//    success: ({ dataprocessed }) => {
-//      if (dataprocessed.result) {
-//        dispatch(dataDelete(state, { id: arg.params.id }));
-//      }
-//    },
-//    failure: ({ dataprocessed, error }) => {
-//      dispatch({ error: dataprocessed || error, id: '' });
-//    }
-//  };
-
-//  base(arg);
-//};
-
-//// richiesta per aggiornare una cultura, indicata precedentemente nella view
-//export const callCulturesPut = async ({ data, dispatch, state }) => {
-//  const arg = {
-//    request: {
-//      id: data.id,
-//      code: data.code,
-//      description: data.description,
-//    },
-//    api: apiCulturePut,
-//    success: ({ dataprocessed }) => {
-//      if (dataprocessed.result) {
-//        dispatch(dataPut(state, arg.request));
-//      }
-//    },
-//    failure: ({ dataprocessed, error }) => {
-//      dispatch({
-//        error: dataprocessed,
-//        id: '',
-//        code: '',
-//        description: '',
-//      });
-//    }
-//  };
-
-//  base(arg);
-//};
--- src/models/api/cultures.model.js
+++ src/models/api/cultures.model.js
@@ -6,37 +6,13 @@
 
 import { base } from '../common/model.base';
 
-
-// percorso dell'api
-//const url = '/api/v1/Cultures';
-
-// Api urls ...
 const URL_CULTURES_LIST = "/api/v1/cultures";
-
-
-// interfaccia api per ottenere la lista corrente delle culture
-//export const apiCultureGet = async ({ headers }) => {
-//  const request = {
-//    method: "get",
-//    headers: {
-//      "Content-Type": "application/json",
-//      ...headers,
-//    },
-//  };
-
-//  return base({ url, request });
-//};
-
-
-
 
 
 // Interface
 export const Cultures = {
     List: (headers) => { return culturesList(headers); }                           // lista dei gruppi
 };
-
-
 
 // Private Methods 
 
@@ -52,47 +28,3 @@ const culturesList = async ({ headers }) => {
 
     return base({ url: URL_CULTURES_LIST, request });
 };
-
-
-
-////////////////////////////////////////////////////////da qui in poi si puo cancellare////////////////////////////////////////////////////////////////////////////////////////7
-// interfaccia api per aggiungere un nuovo elemento alla lista delle culture
-//export const apiCulturePost = async ({ headers, request: data }) => {
-//  const request = {
-//    method: "post",
-//    headers: {
-//      "Content-Type": "application/json",
-//      ...headers,
-//    },
-//    body: JSON.stringify(data),
-//  };
-
-//  return base({ url, request });
-//};
-
-//// interfaccia api per rimuovere un elemento dalla lista delle culture
-//export const apiCultureDelete = async ({ headers, params }) => {
-//  const request = {
-//    method: "delete",
-//    headers: {
-//      "Content-Type": "application/json",
-//      ...headers,
-//    },
-//  };
-
-//  return base({ url: `${url}/:id`, request, params });
-//};
-
-//// interfaccia api per aggiornare un elemento specifico della lista delle culture
-//export const apiCulturePut = async ({ headers, request: data }) => {
-//  const request = {
-//    method: "put",
-//    headers: {
-//      "Content-Type": "application/json",
-//      ...headers,
-//    },
-//    body: JSON.stringify(data),
-//  };
-
-//  return base({ url, request });
-//};

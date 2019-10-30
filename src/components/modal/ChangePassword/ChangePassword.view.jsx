bf7889c58316a227e161ecba19baa0ad1daf90ef
HassanPeoplelab <h.hassan@peoplelab.it>
2019-10-30 17:36:46 +0100

    [Ficep_HMI] SuccessModal for changepassword

--- src/components/modal/ChangePassword/ChangePassword.view.jsx
+++ src/components/modal/ChangePassword/ChangePassword.view.jsx
@@ -8,21 +8,13 @@
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-//import { Modal } from '../../layouts/index.layouts';
-//import { Form, Field, PasswordInput, Submit, Validation, ResetStore } from '../../forms-context/index.form';
 import PasswordInput from '../../forms-context/PasswordInput';
 import { Modal, Table } from '../../layouts/index.layouts';
 import { Form } from '../../forms-context/index.form';
-import { ChangePassword as cChangePassword } from '../../../controllers/routes/users/users.controller';
-import {
-    User as cUser,
-} from '../../../controllers/routes/users/users.controller';
+import { User as cUser} from '../../../controllers/routes/users/users.controller';
 import { ModalHandler } from '../../../controllers/common/modal.handler';
-import { LoggedUser as cLoggedUser } from '../../../controllers/session/loggeduser.controller';
+import history from '../../../models/history/history';
 
-//import { callUsersPassword } from '../../../controllers/routes/users/users.controller';
-
-// import '../../../styles/modal/ErrorModal.style.scss';
 
 
 const initial = {
@@ -94,18 +86,11 @@ class ChangePassword extends PureComponent {
 
         event.preventDefault();
         let data = this.state.currentValues;
-        //const x = cLoggedUser.Get();
-        //const { currentUser } = x;
-        //console.log("x: " + x);
-        //console.log("currentUser: "+currentUser);
-        //data.id= parseInt(currentUser);
-       // cChangePassword({ data });
       
         cUser.ChangePassword({
             data,
             onSuccess: (response) => {
-                ModalHandler.Success();
-               // ModalHandler.Success({ onSuccess: this.closeModal });
+                ModalHandler.Success({ onSuccess: this.closeModal });
                   
             },
             onFailed: (response) => {
@@ -113,63 +98,13 @@ class ChangePassword extends PureComponent {
             }
         });
     }
-    //closeModal = () => {
-    //    this.setState({ showModal: false });
-    //    this.setState({ showModal: true });
-    //}
+    closeModal = () => {
+        history.goBack();
+    }
 
-       // this.onValidation = this.onValidation.bind(this);
-
-
-        //onSubmit(state, event) {
-        //    callUsersPassword({ data: state });
-
-        //    this.setState(prevState => ({ doReset: !(prevState.doReset) }));
-        //}
-
-        //onValidation(state) {
-        //    const { oldPassword, newPassword, confirmPassword } = state;
-        //    return (
-        //        typeof oldPassword !== 'undefined'
-        //        && typeof newPassword !== 'undefined'
-        //        && typeof confirmPassword !== 'undefined'
-        //        && oldPassword !== newPassword
-        //        && confirmPassword === newPassword
-        //    );
-        //}
 
     render() {
-        //  const { doReset } = this.state;
-        {/* <Modal open className="modal--data modal--small password-modal" messages={({ title: this.intl.title })} header="full" footer="none">
-        <Form className="password-modal__form" initial={initial}>
-          <ResetStore doReset={doReset} initial={initial} />
-          <Validation onValidation={this.onValidation} />
-          <div className="password-modal__container">
-            <div className="password-modal__content">
-              <Field className="password-modal__field">
-                <PasswordInput name="oldPassword" placeholder={this.intl.oldPassword} />
-              </Field>
-              <Field className="password-modal__field">
-                <PasswordInput name="newPassword" placeholder={this.intl.newPassword} />
-              </Field>
-              <Field className="password-modal__field">
-                <PasswordInput name="confirmPassword" placeholder={this.intl.confirmPassword} />
-              </Field>
-            </div>
-            <div className="password-modal__content">
-              <Field className="users-modal__field">
-                <Submit name="form-password" required={required} onSubmit={this.onSubmit}>
-                  {this.intl.update}
-                </Submit>
-              </Field>
-            </div>
-          </div>
-        </Form>
-        </Modal>*/}
-
         return (
-
-         //   {  /*   <Modal open={true} className=" modal--data modal--small" messages={({ title: this._labels.title })} header="full" footer="none">*/}
                 <Modal open={true} className=" modal--data modal--small" messages={({ title: this._labels.title })} header="full" footer="none">
                     <Form className="users-modal__form" initial={initial}>
                         <div className="password-modal__container">

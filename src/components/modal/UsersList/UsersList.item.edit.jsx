3f61544e6e15c02c89b5bb58cfb7c990875bdd49
l.fumagalli <l.fumagalli@peoplelab.it>
2019-10-31 12:09:48 +0100

    Modifica select: disabled

--- src/components/modal/UsersList/UsersList.item.edit.jsx
+++ src/components/modal/UsersList/UsersList.item.edit.jsx
@@ -7,45 +7,45 @@ import PropTypes from 'prop-types';
 
 
 /*
-	* Componente EDITITEM. Modifica di un utente.
-	* Props:
-	* - groups : elenco dei gruppi/ruoli di un utente.
-	* - currentUser : utente "corrente" (selezionato), json composto da:
-	*                                                      - id: 0
-														- firstName: "",
-														- lastName: "",
-														- password: "",
-														- groups: null,
-														- isLocked: false
+	Componente EDITITEM. Modifica di un utente.
+	Props:
+	- groups : elenco dei gruppi/ruoli di un utente.
+	- currentUser : utente "corrente" (selezionato), json composto da:
+		- id: 0
+		- firstName: "",
+		- lastName: "",
+		- password: "",
+		- groups: null,
+		- isLocked: false
 	- onSave : gestore del salvataggio utente
 */
 
 
 class EditItem extends Component {
-    // etichette in lingua
-    _labels = {
-        firstName: window.intl.users_field_firstname,
-        lastName: window.intl.users_field_lastname,
-       // password: window.intl.users_field_password,
-        group: window.intl.users_field_role,
-        groups: {
-            ADMIN: window.intl.users_role_administrator,
-            SUPERUSER: window.intl.users_role_technician,
-            USER: window.intl.users_role_operator
-        },
-        save: window.intl.users_field_save,
-        reset: window.intl.users_field_reset,
-    };
-    // elenco elementi (options) del dropdown gruppi/ruoli
-    _groups = [];
-    // valori default (vuoti)
-    _emptyValues = {
-        Id: 0,
-        FirstName: "",
-        LastName: "",
-        Groups: null,
-        isLocked: false
-    }
+	// etichette in lingua
+	_labels = {
+		firstName: window.intl.users_field_firstname,
+		lastName : window.intl.users_field_lastname,
+		group    : window.intl.users_field_role,
+		save     : window.intl.users_field_save,
+		reset    : window.intl.users_field_reset,
+		groups   : {
+			ADMIN: window.intl.users_role_administrator,
+			SUPERUSER: window.intl.users_role_technician,
+			USER: window.intl.users_role_operator
+		},
+	};
+	// elenco elementi (options) del dropdown gruppi/ruoli
+	_groups = [];
+
+	// valori default (vuoti)
+	_emptyValues = {
+		Id: 0,
+		FirstName: "",
+		LastName: "",
+		Groups: null,
+		isLocked: false
+	}
 
 
 	constructor(props) {
@@ -63,9 +63,9 @@ class EditItem extends Component {
 		this._groups = this.getCodeOptions(props.groups);
 	}
 
-    componentWillReceiveProps(nextProps) {
-        // se currentUser di nextprops è null assegno i valori dello state a emptyValues, per evitare che ci siano errori nel "render".
-        const user = (nextProps.currentUser.FirstName === null || (typeof nextProps.currentUser.FirstName === "undefined")) ? this._emptyValues : nextProps.currentUser;
+	componentWillReceiveProps(nextProps) {
+		// se currentUser di nextprops è null assegno i valori dello state a emptyValues, per evitare che ci siano errori nel "render".
+		const user = (nextProps.currentUser.FirstName === null || (typeof nextProps.currentUser.FirstName === "undefined")) ? this._emptyValues : nextProps.currentUser;
 
 		this._groups = this.getCodeOptions(nextProps.groups);
 		this.setState({ originalValues: user, currentValues: user });
@@ -91,17 +91,17 @@ class EditItem extends Component {
 
 		const value = event.target.value;
 
-        if (event.target.name.indexOf("lastName") >= 0) {
-            currentValues = { ...this.state.currentValues, "LastName": value };
-        } else if (event.target.name.indexOf("firstName") >= 0) {
-            currentValues = { ...this.state.currentValues, "FirstName": value };
-        } /*else if (event.target.name.indexOf("password") >= 0) {
-            currentValues = { ...this.state.currentValues, "password": value };
-        }*/ else if (event.target.name.indexOf("group") >= 0) {
-            currentValues = { ...this.state.currentValues, "Groups": [{ "id": value }] };
-        } else {
-            currentValues = this.state.currentValues;
-        }
+		if (event.target.name.indexOf("lastName") >= 0) {
+			currentValues = { ...this.state.currentValues, "LastName": value };
+		} else if (event.target.name.indexOf("firstName") >= 0) {
+			currentValues = { ...this.state.currentValues, "FirstName": value };
+		} /*else if (event.target.name.indexOf("password") >= 0) {
+			currentValues = { ...this.state.currentValues, "password": value };
+		}*/ else if (event.target.name.indexOf("group") >= 0) {
+			currentValues = { ...this.state.currentValues, "Groups": [{ "id": value }] };
+		} else {
+			currentValues = this.state.currentValues;
+		}
 
 		this.setState({ currentValues });
 	};
@@ -123,34 +123,30 @@ class EditItem extends Component {
 		const { errorCase } = this.props;
 		//  const password_classname = "users-modal__field " + ((this.state.currentValues.isLocked) ? "readonly" : "show");
 
-        const firstName = this.state.currentValues.FirstName || ""; // || "" serve per evitare un warning di react (A component is changing an uncontrolled input of type text to be controlled).
-        const lastName = this.state.currentValues.LastName || "";   // || "" serve per evitare un warning di react (A component is changing an uncontrolled input of type text to be controlled).
-        const selectedGroup = (this.state.currentValues.Groups == null) ? "-1" : this.state.currentValues.Groups[0].id;
-        const classSelect = "input input__select " + ((this.state.currentValues.Id === 0) ? "" : "readonly");
+		const firstName     = this.state.currentValues.FirstName || ""; // || "" serve per evitare un warning di react (A component is changing an uncontrolled input of type text to be controlled).
+		const lastName      = this.state.currentValues.LastName || "";   // || "" serve per evitare un warning di react (A component is changing an uncontrolled input of type text to be controlled).
+		const selectedGroup = (this.state.currentValues.Groups == null) ? "-1" : this.state.currentValues.Groups[0].id;
+		const disableSelect = ((this.state.currentValues.Id === 0) ? "" : "disabled");
 
 		const classFirstName = "field users-modal__field " + (errorCase.includes('USER_MANAGEMENT_FIRSTNAME_EMPTY') ? "field--error" : "");
-		const classLastName = "field users-modal__field " + (errorCase.includes('USER_MANAGEMENT_LASTNAME_EMPTY') ? "field--error" : "");
-		const classGroups = "field users-modal__field " + (errorCase.includes('USER_MANAGEMENT_GROUPS_NOTSPECIFIED') ? "field--error" : "");
+		const classLastName  = "field users-modal__field " + (errorCase.includes('USER_MANAGEMENT_LASTNAME_EMPTY') ? "field--error" : "");
+		const classGroups    = "field users-modal__field " + (errorCase.includes('USER_MANAGEMENT_GROUPS_NOTSPECIFIED') ? "field--error" : "");
 
 		return (
 			<>
 				{/* Campo Cognome */}
 				<div className={classFirstName}>
-					<TextInput className="input-large" name="firstName" placeholder={this._labels.firstName} value={firstName} onChange={this.handleInputChange} required />
+					<TextInput className="input-large" name="firstName" placeholder={this._labels.firstName} value={firstName} onChange={this.handleInputChange} />
 				</div>
 				
 				{/* Campo Nome */}
 				<div className={classLastName}>
 					<TextInput className="input-large" name="lastName" placeholder={this._labels.lastName} value={lastName} onChange={this.handleInputChange} />
 				</div>
-
-				{/*<div className={password_classname}>
-					<PasswordInput name="password" placeholder={this._labels.password} onChange={this.handleInputChange} />
-				</div>*/}
 				
 				{/* Campo Ruolo */}
 				<div className={classGroups}>
-					<Select className="select-large" name="group" value={selectedGroup} onChange={this.handleInputChange}>
+					<Select className={`select-large ${disableSelect}`} name="group" value={selectedGroup} onChange={this.handleInputChange} disabled={disableSelect}>
 						{this._groups}
 					</Select>
 				</div>
@@ -173,14 +169,14 @@ class EditItem extends Component {
 
 
 
-/**
-	* Define component properties types
-	*/
+// 
+// ** Define component properties types **
+//
 EditItem.propTypes = {
-	groups: PropTypes.arrayOf(PropTypes.object).isRequired,
+	groups     : PropTypes.arrayOf(PropTypes.object).isRequired,
 	currentUser: PropTypes.object.isRequired,
-	onSave: PropTypes.func.isRequired,
-	errorCase: PropTypes.arrayOf(PropTypes.string).isRequired,
+	onSave     : PropTypes.func.isRequired,
+	errorCase  : PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 

0b674a25da941ba3b8410c9083928c860fcb63bb
lmartignoni <l.martignoni@peoplelab.it>
2019-11-05 14:12:54 +0100

    [FicepHMI] Updated LoggedUser module. Added FirstName and LastName.

--- src/controllers/session/loggeduser.controller.js
+++ src/controllers/session/loggeduser.controller.js
@@ -1,12 +1,29 @@
 //----------------------------------------------------------------------------------------
-// File: user.controller.js
+// File: loggeduser.controller.js
 //
 // Controller per la gestione dell'utente loggato (in sessione / store).
+// ----------------------------------------------------------------------
+// Dati Contenuti:
+//
+// AccessToken: <string>
+// Culture: "it-IT"
+// ExpiredAt: "2019-11-05T13:10:06.7130000+01:00"
+// Groups: ["ADMIN"]
+// IssuedAt: "2019-11-05T11:30:06.7130000+01:00"
+// Permissions: ["GROUP_MANAGEMENT", "TOOL_MANAGEMENT", "USER_MANAGEMENT", "VIEW_GROUP", "VIEW_GROUPS", "VIEW_TOOL",…]
+// RefreshExpiredAt: "2019-11-05T14:50:06.7130000+01:00"
+// RefreshToken: "3c4a9690-ffb7-11e9-a163-e7b318f638a5"
+// SessionId: "22c8a3f8-fe91-4956-8add-4ff98b91de54"
+// SessionLogId: 10
+// UserId: 1
+// Username: "admin"
+// FirstName: ""
+// LastName: ""
 //----------------------------------------------------------------------------------------
 
 import store from '../../store/redux.store';
 import { types as storeTypes } from '../../store/session.store';
-
+import { Users as mUsers } from '../../models/api/users.model';
 
 
 // Interface
@@ -27,18 +44,55 @@ const getLoggedUser = () => store.getState().session.LoggedUser;
 
 
 // imposta l'oggetto loggeduser contenuto nello store
-const setLoggedUser = (loggedUser) => {
+const setLoggedUser = async (loggedUser) => {
+
+
+    // bisogna completare i dati provenienti dalla login con quelli del dettaglio utente...
+    const { firstName, lastName } = await getAdditionalInfo(loggedUser);
+
+    const payload = {
+        ...loggedUser,
+        FirstName: firstName,
+        LastName: lastName
+    };
 
     store.dispatch({
         type: storeTypes.LOGGED_USER,
-        payload: loggedUser,
+        payload: payload,
     });
 
     // da cancellare non serve, resta solo per far funzionare tutto il resto...
     store.dispatch({
         type: storeTypes.LOGGED_USER_TOBEDELETED,
-        payload: loggedUser,
+        payload: payload,
     });
+
+};
+
+const getAdditionalInfo = async (loggedUser) => {
+
+    const id = loggedUser.userId;
+
+    const request = {};
+    const params = { id };
+    const headers = {
+        Authorization: 'Bearer ' + loggedUser.accessToken,
+        Session: loggedUser.sessionId,
+    };
+    const response = mUsers.Detail({ request, headers, params });
+    const { httpcode, dataraw } = await response;
+
+    let firstName = "";
+    let lastName = "";
+    if (httpcode === 200) {
+        const json_dataraw = JSON.parse(dataraw);
+        firstName = json_dataraw.Result.FirstName;
+        lastName = json_dataraw.Result.LastName;
+    }
+
+    return {
+        firstName, lastName
+    };
 };
 
 

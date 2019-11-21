01e5b6992bacfd01a08ef8f27b750dacda8e66ea
Dev01 <dev01@peoplelab.it>
2019-11-21 11:50:48 +0100

    Modifiche fogli CSS e prima revisione codice JSX

--- src/components/modal/UsersList/UsersList.item.row.jsx
+++ src/components/modal/UsersList/UsersList.item.row.jsx
@@ -1,149 +1,164 @@
-
-/*
-	* Componente ROWITEM. Riga dettaglio di un utente.
-	* Props:
-	* - value : utente "corrente" (da visualizzare), json composto da:
-	*                                                      - id: 0
-														- firstName: "",
-														- lastName: "",
-														- username: "",
-														- isActive: true,
-														- isLocked: false,
-														- creationDate: date,
-														- groups: null,
-	* - index : indice di riga (non utilizzato, da vedere....)
-	- onEdit : evento "bottone Modifica cliccato" ("data" contiene l'id utente)
-	- onDelete : evento "bottone Elimina cliccato" ("data" contiene l'id utente)
-*/
-
-
-
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { ButtonData } from '../../layouts/index.layouts';
-import { ModalHandler } from '../../../controllers/common/modal.handler';
-import  ResetPassword  from '../../../widgets/common/ResetPassword';
-
-//import '../../../styles/modal/UsersList.style.scss';
-
-
-class RowItem extends Component {
-
-    // etichette in lingua
-    _labels = {
-        yes: window.intl.users_field_enable,
-        no: window.intl.users_field_disable,
-        update: window.intl.users_field_update,
-        delete: window.intl.users_field_delete,
-        groups: {
-            ADMIN: window.intl.users_role_administrator,
-            SUPERUSER: window.intl.users_role_technician,
-            USER: window.intl.users_role_operator
-        },
-    };
-
-    constructor(props) {
-        super(props);
-    }
-
-    onSelect() { console.log(this.props); }
-    onSuccess = (response) => {
-        console.log(response.newPassword);
-        ModalHandler.Info({ message: [' Password: ' + response.newPassword] });
-    }
-    onFailed = (response) => { ModalHandler.Error({ errorCode: response.dataprocessed.errorCode, errorsList: response.dataprocessed.result });}
-    render() {
-        const { value, index } = this.props;
-        const {
-            id,
-            firstName,
-            lastName,
-            userName,
-            //   isActive,
-            //  isLocked,
-            userStatus,
-            creationDate,
-            groups,
-        } = value;
-
-       
-        const [{ code }] = groups;
-
-        // const active = isActive ? this._labels.yes : this._labels.no;
-        const active = (userStatus == 1) ? this._labels.yes : this._labels.no;
-        const creation_date = creationDate.split(/T|\..*/).join(' ');
-
-        const editbtn_classname = "btn btn-std btn-light " + ((userStatus == 1 || userStatus == 2) ? "show" : "hidden");
-        const deletebtn_classname = "btn btn-std btn-midGrey " + ((userStatus == 1) ? "show" : "hidden");
-
-        return (
-            <Fragment key={`table-row-${index}`} >
-                {/* Cognome */}
-                <td className="cell-std">{firstName}</td>
-                {/* Nome */}
-                <td className="cell-std">{lastName}</td>
-                {/* Username */}
-                <td className="cell-std bold">{userName}</td>
-
-                <td className="cell-std textCenter">
-                    <ButtonData className="btn btn-std btn-midGrey" data={value} onClick={this.props.onActive} >
-                        {active}
-                    </ButtonData>
-                </td>
-                {/* Data di creazione */}
-                <td className="cell-std textCenter">{creation_date}</td>
-                {/* Ruolo */}
-                <td className="cell-std">{this._labels.groups[code]}</td>
-                {/* Pulsante "ResetPassword" */}
-                <td className="cell-std">
-                    <ResetPassword className="" data={id} onSuccess={this.onSuccess} onFailed={this.onFailed}/>
-                </td>
-
-                {/* Pulsante "Modifica" */}
-                <td className="cell-std">
-                    <ButtonData className={editbtn_classname} data={id} onClick={this.props.onEdit} >
-                        {this._labels.update}
-                    </ButtonData>
-                </td>
-                {/* Pulsante "Elimina" */}
-                <td className="cell-std">
-                    <ButtonData className={deletebtn_classname} data={id} onClick={this.props.onDelete} >
-                        {this._labels.delete}
-                    </ButtonData>
-                </td>
-            </Fragment>
-        );
-    }
-}
-
-
-const shapeValue = {
-    firstName: PropTypes.string.isRequired,
-    lastName: PropTypes.string.isRequired,
-    userName: PropTypes.string.isRequired,
-    //	isActive    : PropTypes.bool.isRequired,
-    userStatus: PropTypes.number.isRequired,
-    creationDate: PropTypes.string.isRequired,
-    groups: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
-
-
+//--------------------------------------------------------------------------
+// File: UsersList.item.row.jsx
+// Desc: Definizione componente ROWITEM. Riga dettaglio di un utente.
 //
-// * Define component properties types *
-//
-RowItem.propTypes = {
-    value: PropTypes.shape(shapeValue).isRequired,
-    index: PropTypes.number.isRequired,
-    onActive: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onEdit: PropTypes.func.isRequired,
-};
+// Path: /src/components/modals/UserList/
+//--------------------------------------------------------------------------
 
-//
-// * Define default value of component properties *
-//
-RowItem.defaultProps = {
-};
+	// ** Scheda Componente **
+	// NAME : Rowitem > Riga dettaglio di un utente
+	// PROPS:
+	//	value    : utente "corrente" (da visualizzare), json composto da:
+	//    .id           : 0
+	//    .firstName    : "",
+	//    .lastName     : "",
+	//    .username     : "",
+	//    .isActive     : true,
+	//    .isLocked     : false,
+	//    .creationDate : date,
+	//    .groups       : null,
+	//	index    : indice di riga (non utilizzato, da vedere....)
+	//	onEdit   : evento "bottone Modifica cliccato" ("data" contiene l'id utente)
+	//	onDelete : evento "bottone Elimina cliccato" ("data" contiene l'id utente)
 
 
-export default RowItem;
+
+	// ** Definizione import **
+	import React, { Component, Fragment } from 'react';													// Basic: import React
+
+	import PropTypes      from 'prop-types';
+//	import { ButtonData } from '../../layouts/index.layouts';
+	import ButtonData     from '../../layouts/ButtonData';
+	import {ModalHandler} from '../../../controllers/common/modal.handler';
+	import ResetPassword  from '../../../widgets/common/ResetPassword';
+	//import '../../../styles/modal/UsersList.style.scss';
+
+
+	// ** Definizione componente **
+	class RowItem extends Component {
+
+	//	constructor(props) {																			// Costruttore (In questo caso non serve. Perché?)
+	//		super(props);
+	//	}
+
+
+		// ** Definizione testi delle etichette nella lingua corrente **
+		_labels = {
+			yes   : window.intl.users_field_enable,
+			no    : window.intl.users_field_disable,
+			update: window.intl.users_field_update,
+			delete: window.intl.users_field_delete,
+			groups: {
+				ADMIN    : window.intl.users_role_administrator,
+				SUPERUSER: window.intl.users_role_technician,
+				USER     : window.intl.users_role_operator
+			},
+		};
+
+
+		// ** Definizione handler eventi **
+		onSuccess = (response) => {																		// Evento: Success
+			console.log('New Password: ', response.newPassword);
+			ModalHandler.Info({																			// Richiama la dialog box "info" visualizzando la nuova password
+				message: [' Password: ' + response.newPassword]
+			});
+		}
+		onFailed = (response) => {																		// Evento: Failed
+			ModalHandler.Error({																		// Richiama la dialog box "error"
+				errorCode : response.dataprocessed.errorCode, 
+				errorsList: response.dataprocessed.result
+			});
+		}
+		
+		
+		// ** Funzione di Render del componente **
+		render() {
+
+			// Definizione costanti
+			const { value, index } = this.props;														// Lettura valori delle Props
+			const {
+				id,
+				firstName,
+				lastName,
+				userName,
+				userStatus,
+				creationDate,
+				groups,
+			} = value;
+
+			const [{ code }]          = groups;
+			const active              = (userStatus == 1) ? this._labels.yes : this._labels.no;
+			const creation_date       = creationDate.split(/T|\..*/).join(' ');
+			const editbtn_classname   = "btn-std-full btn-light "   + ((userStatus == 1 || userStatus == 2) ? "show" : "hidden");
+			const deletebtn_classname = "btn-std-full btn-midGrey " + ((userStatus == 1) ? "show" : "hidden");
+
+			return (
+				<Fragment key={`table-row-${index}`} >
+
+					{/* Cognome */}
+					<td className="cell-std">{firstName}</td>
+
+					{/* Nome */}
+					<td className="cell-std">{lastName}</td>
+
+					{/* Username */}
+					<td className="cell-std bold">{userName}</td>
+
+					{/* Attivazione */}
+					<td className="cell-std textCenter">
+						<ButtonData className="btn btn-std btn-midGrey" data={value} onClick={this.props.onActive} >
+							{active}
+						</ButtonData>
+					</td>
+				
+					{/* Data di creazione */}
+					<td className="cell-std textCenter">{creation_date}</td>
+				
+					{/* Ruolo */}
+					<td className="cell-std">{this._labels.groups[code]}</td>
+
+					{/* Pulsante "ResetPassword" */}
+					<td className="cell-std">
+						<ResetPassword className="btn btn-std-full btn-dark" data={id} onSuccess={this.onSuccess} onFailed={this.onFailed}/>
+					</td>
+
+					{/* Pulsante "Modifica" */}
+					<td className="cell-std">
+						<ButtonData className={editbtn_classname} data={id} onClick={this.props.onEdit} >
+							{this._labels.update}
+						</ButtonData>
+					</td>
+					{/* Pulsante "Elimina" */}
+					<td className="cell-std">
+						<ButtonData className={deletebtn_classname} data={id} onClick={this.props.onDelete} >
+							{this._labels.delete}
+						</ButtonData>
+					</td>
+
+				</Fragment>
+			);
+		}
+	}
+
+
+	// ** Definizioni delle Props **
+	const shapeValues = {																				// Definizione parz.: tipi degli elementi della Prop "value"
+		firstName   : PropTypes.string.isRequired,
+		lastName    : PropTypes.string.isRequired,
+		userName    : PropTypes.string.isRequired,
+		userStatus  : PropTypes.number.isRequired,
+		creationDate: PropTypes.string.isRequired,
+		groups      : PropTypes.arrayOf(PropTypes.object).isRequired,
+	};
+	RowItem.propTypes = {																				// Definizione dei tipi delle Props del componente
+		value   : PropTypes.shape(shapeValues).isRequired,
+		index   : PropTypes.number.isRequired,
+		onActive: PropTypes.func.isRequired,
+		onDelete: PropTypes.func.isRequired,
+		onEdit  : PropTypes.func.isRequired,
+	};
+
+	RowItem.defaultProps = {};																			// Definizione valori di default delle Props componente
+
+
+	export default RowItem;																				// Dichiarazione finale di export della classe componente
